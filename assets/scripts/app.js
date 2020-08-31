9408bc30b42cb8299262ac6c73859f32410483f4
ashumsd7 <40313523+ashumsd7@users.noreply.github.com>
2020-08-31 12:54:41 +0530

    first commit

---
+++ assets/scripts/app.js
@@ -0,0 +1,79 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = []; //logs array
+
+//getting input from user input
+function getUserNumberInput() {
+  return parseInt(usrInput.value);
+}
+
+//generates and write calculation log and result
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator} ${calcNumber}`;
+  outputResult(currentResult, calcDescription); // from vendor.js
+}
+
+//writing log common function
+function writeToLog(
+  operationIndentifier,
+  prevResult,
+  operationNumber,
+  newResult
+) {
+  const logEntry = {
+    operation: operationIndentifier,
+    prevResult: prevResult,
+    number: operationNumber,
+    result: newResult,
+  };
+  logEntries.push(logEntry);
+  console.log(logEntry.operation);
+  console.log(logEntries.reverse());
+}
+
+function add( ) {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult + enteredNumber;
+  createAndWriteOutput("+", initialResult, enteredNumber);
+  writeToLog('ADD',initialResult,enteredNumber,currentResult)
+}
+
+function subtract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult - enteredNumber;
+  createAndWriteOutput("-", initialResult, enteredNumber);
+  writeToLog('SUBTRACT',initialResult,enteredNumber,currentResult)
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult * enteredNumber;
+  createAndWriteOutput("*", initialResult, enteredNumber);
+  writeToLog('MULTIPLY',initialResult,enteredNumber,currentResult)
+}
+
+function devide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult = currentResult / enteredNumber;
+  createAndWriteOutput("/", initialResult, enteredNumber);
+  writeToLog('DEVIDE',initialResult,enteredNumber,currentResult)
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", subtract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", devide);
+
+// const additionResult= add(1,20)
+
+// currrentResult = defaultResult;
+// let calculationDescription = "(" + defaultResult+"+10)"
+//how u can use backticks and show varNames using doolor $ sign and curlybraces {}
+// let calculationDescription = `(  ${defaultResult} + 10)`;
+
+// let errorMesage= 'an error\n' +
+//                     'coocured';
